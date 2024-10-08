eae7c01713a5a1c74b597465f2c24ec283443afe
gabriegord <159156990+gabriegord@users.noreply.github.com>
2024-10-08 12:32:08 -0300

    Add files via upload
    07/10/24

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+'Programção',
+'O que é Javascript',
+'O JavaScript é uma linguagem de programação?'
+)
+
+criarCartão(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+cirarcartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de códigos que executa uma  funçã'
+)
